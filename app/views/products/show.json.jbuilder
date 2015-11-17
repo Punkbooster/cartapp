f917b815137da11e1788757fa51b2d1b622360c0
Punkbooster <2morrow2late@rambler.ru>
2015-11-18 00:56:05 +0100

    Remove category attribute from products

--- app/views/products/show.json.jbuilder
+++ app/views/products/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @product, :id, :title, :description, :price, :category, :created_at, :updated_at
+json.extract! @product, :id, :title, :description, :price, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117234204) do
+ActiveRecord::Schema.define(version: 20151117235356) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "price"
-    t.string   "category"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
