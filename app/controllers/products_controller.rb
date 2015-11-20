7ff263c1a11f4e757209039f23dab47669bae0a9
Punkbooster <2morrow2late@rambler.ru>
2015-11-20 17:03:26 +0100

    Add images for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -60,6 +60,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:title, :description, :price, :category_id)
+      params.require(:product).permit(:title, :description, :price, :category_id, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118135957) do
+ActiveRecord::Schema.define(version: 20151120153737) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -23,9 +23,13 @@ ActiveRecord::Schema.define(version: 20151118135957) do
     t.string   "title"
     t.text     "description"
     t.integer  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "category_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
